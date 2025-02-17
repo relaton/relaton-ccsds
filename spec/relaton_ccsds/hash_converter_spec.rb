03f1f5ca1e893749069a843cda8001bb2d5517bc
Andrei Kislichenko <android.2net@gmail.com>
2025-02-17 15:20:09 -0500

    WIP create flavor model

--- spec/relaton_ccsds/hash_converter_spec.rb
+++
@@ -1,6 +0,0 @@
-describe RelatonCcsds::HashConverter do
-  it "returns CCSDS bibliographic item" do
-    item = described_class.bib_item title: ["title"]
-    expect(item).to be_instance_of RelatonCcsds::BibliographicItem
-  end
-end
